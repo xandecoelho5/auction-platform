44f29e81db89102e4fd7c8feca2b42ceb14a2b44
xandecoelho5 <xandecoelho5@hotmail.com>
2023-02-28 01:37:19 -0300

    added base project

---
+++ src/test/java/com/xandecoelho5/auctionplatform/AuctionPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xandecoelho5.auctionplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
